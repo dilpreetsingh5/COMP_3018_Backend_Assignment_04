d7ba9c619b6796ad2f963bfa1152164325383c87
dilpreetsingh5 <dilpreetsingh5@example.com>
2025-11-02 23:27:04 -0600

    feat: add user and admin routes for retrieving user details and setting custom claims

---
+++ src/api/v1/routes/userRoutes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { getUserHandler, setCustomClaimsHandler } from "../controllers/userController";
+import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize";
+
+const router: express.Router = express.Router();
+
+// User routes
+router.get("/users/:uid", authenticate, getUserHandler);
+
+// Admin routes
+router.post(
+    "/admin/setCustomClaims",
+    authenticate,
+    isAuthorized({ hasRole: ["admin"] }),
+    setCustomClaimsHandler
+);
+
+export default router;
