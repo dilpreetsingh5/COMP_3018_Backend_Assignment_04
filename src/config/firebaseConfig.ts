a733fb0b7afd6e00632d81b997ab79b939bc660b
dilpreetsingh5 <dilpreetsingh5@example.com>
2025-11-04 12:59:36 -0600

    update firebaseConfig file

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getAuth, Auth } from "firebase-admin/auth";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-const serviceAccount = require("../../Security_Key.json") as ServiceAccount; 
+import serviceAccount from "../../Security_Key.json"; 
 
 initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
